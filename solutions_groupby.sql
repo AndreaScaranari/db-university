1329d9b8955aabe988575366831a858ca0e7a24a
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-01 17:39:12 +0100

    groupby_4

--- solutions_groupby.sql
+++ solutions_groupby.sql
@@ -13,3 +13,5 @@ WHERE `vote` > 17
 GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(`id`) AS `degrees_in_department` FROM `degrees` 
+GROUP BY `department_id`;
