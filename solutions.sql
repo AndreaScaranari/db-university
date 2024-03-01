fefc92b772c9d7bfb03e0d9fab2c286264c8e270
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-01 17:09:19 +0100

    select_7

--- solutions.sql
+++ solutions.sql
@@ -32,4 +32,7 @@ SELECT `department_id`, `name`, `address` FROM `degrees`
 WHERE `name` LIKE '%Magistrale%';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT `id`, `name` FROM `departments`
+GROUP BY `id`;
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
