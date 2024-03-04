d12e0901fcc4cbdfce3a9fe7cdfbb2930177150e
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-04 18:34:57 +0100

    minor_fixes

--- solutions_groupby.sql
+++ solutions_groupby.sql
@@ -1,17 +1,21 @@
 -- * Esercizi con GROUP BY
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-SELECT YEAR(`enrolment_date`) AS `year`, COUNT(`id`) AS `new_students` FROM `students` 
+SELECT YEAR(`enrolment_date`) AS `year`, COUNT(`id`) AS `new_students` 
+FROM `students` 
 GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT `office_address`, COUNT(`id`) AS `teachers_in_same_address` FROM `teachers` 
+SELECT `office_address`, COUNT(`id`) AS `teachers_in_same_address` 
+FROM `teachers` 
 GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-SELECT `exam_id`, ROUND(AVG(`vote`), 2) AS `average_vote` FROM `exam_student`
+SELECT `exam_id`, ROUND(AVG(`vote`), 2) AS `average_vote` 
+FROM `exam_student`
 WHERE `vote` > 17
 GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT `department_id`, COUNT(`id`) AS `degrees_in_department` FROM `degrees` 
+SELECT `department_id`, COUNT(`id`) AS `degrees_in_department` 
+FROM `degrees` 
 GROUP BY `department_id`;
--- solutions_select.sql
+++ solutions_select.sql
@@ -3,15 +3,27 @@
 SELECT `name`, `surname`, `registration_number`, `date_of_birth` FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
+-- # alternativa (faster)
+SELECT `name`, `surname`, `registration_number`, `date_of_birth` FROM `students`
+WHERE `date_of_birth` LIKE '1990-%';
+
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT `name`, `period`, `year`, `cfu` FROM `courses` 
 WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`, `surname`, `registration_number`, `date_of_birth`, `fiscal_code` FROM `students` 
+WHERE `date_of_birth` < '1994-03-02';
+
+-- # con TIMESTAPDIFF
+SELECT `name`, `surname`, `registration_number`, `date_of_birth`, `fiscal_code` FROM `students` 
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
+
+-- # alternativa (bruttina)
+SELECT `name`, `surname`, `registration_number`, `date_of_birth`, `fiscal_code` FROM `students` 
 WHERE DAY(`date_of_birth`) < 2 
 AND MONTH(`date_of_birth`) < 3 
-AND YEAR(`date_of_birth`) < 1995;
+AND YEAR(`date_of_birth`) < 1994;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
@@ -22,10 +34,8 @@ AND `year` = 1;
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 -- 20/06/2020 (21)
 SELECT * FROM `exams`
-WHERE `hour` >= "14:00:00"
-AND DAY(`date`) = 20
-AND MONTH(`date`) = 06
-AND YEAR(`date`) = 2020;
+WHERE `date` = '2020-06-20'
+AND HOUR(`hour`) >= '14';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT `department_id`, `name`, `address` FROM `degrees` 
@@ -39,9 +49,5 @@ SELECT `id`, `name` FROM `departments`
 GROUP BY `id`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT COUNT(`id`) AS `teachers_withouth_phone` FROM `teachers` WHERE `phone` <> '%';
-
--- # con visualizzazione grafica
-SELECT `id`, `surname`, `email`, `office_number` FROM `teachers`
-WHERE `phone` <> '%';
-
+SELECT COUNT(`id`) AS `teachers_withouth_phone` FROM `teachers` 
+-- WHERE `phone` IS NULL;
